ef6e883b44695b44efcf8dd0b26ff5590ae8e945
Eric Dube <eric.alex.dube@gmail.com>
2017-02-04 13:29:35 -0500

    Prepare for npm

--- build.sh
+++ build.sh
@@ -8,18 +8,18 @@
 # If you develop of less platform-dependant solution please
 # submit a pull request.
 
-# Make ./processed if it doesn't exist
-mkdir -p ./processed
+# Make ./build if it doesn't exist
+mkdir -p ./build
 echo "// build.sh needs at least one file" > ./source/garbage_js.js
 echo "// build.sh needs at least one file" > ./source/garbage_json.json
 
 # Compile CoffeeScript source
 echo "[1] Processing CoffeeScript files"
-coffee --compile --output processed/ source/
+coffee --compile --output build/ source/
 # Move over any JavaScript source
 echo "[2] Copying pure source"
 cd source
-cp --parents `find -name \*.js` ../processed/
-cp --parents `find -name \*.json` ../processed/
+cp --parents `find -name \*.js` ../build/
+cp --parents `find -name \*.json` ../build/
 cd ..
 echo "[Done]"
